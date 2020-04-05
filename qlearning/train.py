3d34b1c7f01180e88b9079b77fcdf15c487fed62
gagaxiaolong <gagaxiaolong@gmail.com>
2020-04-06 00:20:16 +0800

    log fix

--- qlearning/train.py
+++ qlearning/train.py
@@ -26,23 +26,21 @@ qt = QLearningTable()
 observation = fb.next_frame(random.choice([0, 1]))[-1]
 
 score = 0
-max_score = 0
+# max_score = 0
 episode = 0
 while 1:
     qt.check_state_exist(observation)
     text = "score: {} {} : {:.3f},{:.3f}".format(score, observation, *qt.q_table[observation])
     action = qt.choose_action(observation)
     reward, terminal, score, observation_ = fb.next_frame(action, text)
-    if score > max_score:
-        max_score = score
 
     if terminal:
         observation_ = 'terminal'
         qt.learn(observation, action, reward, observation_)
         episode += 1
-        if (episode) % 10 == 0:
-            logger.info('Episode: {}, Max_score: {}, States: {}'.format(episode, max_score, qt.q_table.__len__()))
-            json.dump(qt.q_table, open('qtable.json', 'w'), sort_keys=True, indent=4)
+        # if (episode) % 10 == 0:
+        logger.info('Episode: {}, Max_score: {}, States: {}'.format(episode, score, qt.q_table.__len__()))
+            # json.dump(qt.q_table, open('qtable.json', 'w'), sort_keys=True, indent=4)
     else:
         qt.learn(observation, action, reward, observation_)
         observation = observation_
